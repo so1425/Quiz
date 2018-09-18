79c60d9406ad82d1e713bd18ed21b2491489378f
so1425 <rhc-myao425os@ezweb.ne.jp>
2018-09-18 18:22:16 +0900

    Quiz

---
+++ Quiz/ResultViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ResultViewController.swift
+//  Quiz
+//
+//  Created by so1425 on 2018/09/18.
+//  Copyright © 2018年 so1425. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var correctAnswer: Int = 0
+    
+    @IBOutlet var resultLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        resultLabel.text = String(correctAnswer)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func back() {
+        self.presentingViewController?.presentingViewController?
+            .dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
